941e2b9fd8cbbfe994431c6ecc3a7dc67fca1e9f
AMendoza <allanmaleman@gmail.com>
2026-07-26 12:04:14 -0600

    FIX: Se implementan algunas mejoras en el sistema de publicacion y de reportes de log para tener una bitacora de todos los procesos realizados

--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ DST="/home/u701829741/domains/allanmendoza.tech/public_html/"
   echo "=== $(date) ==="
   echo "Origen: $SRC"
   echo "Destino: $DST"
-  if rsync -av --delete "$SRC" "$DST" >> "$LOG" 2>&1 ; then
+  if rsync -av "$SRC" "$DST" >> "$LOG" 2>&1 ; then
     echo "Deploy OK"
   else
     echo "Deploy Failed"
